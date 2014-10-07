ec65c8371fb414a505067b1760c57d453a619316
a2016rshah <2016rshah@gmail.com>
2014-10-07 02:12:46 +0000

    Added a new animation!

--- helloworld/client/router.js
+++ helloworld/client/router.js
@@ -5,6 +5,7 @@ Router.map(function(){
     this.route('animations');
     this.route('animation1', {path: '/animations/animation1'});
     this.route('animation2', {path: '/animations/animation2'});
+    this.route('animation3', {path: '/animations/animation3'});
     this.route('home', {path: '/'} );
     console.log(document.URL);
     
